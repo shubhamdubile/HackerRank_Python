bef02c2ea3cf8f7e091293f2c55596f4e406eb78
shubhamdubile <dubileshubham@gmail.com>
2020-06-21 22:40:33 +0530

    Array_Mathematics

---
+++ Numpy/Array_Mathematics.py
@@ -0,0 +1,10 @@
+import numpy
+N,M = map(int,input().split())
+A = numpy.array([input().split() for _ in range (0,N)],int)
+B = numpy.array([input().split() for _ in range (0,N)],int)
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+print(A**B)
