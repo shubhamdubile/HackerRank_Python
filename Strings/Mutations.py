9fea0d8f04ff7c0d65b7b90eda54778432695fbd
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2021-10-24 14:24:59 +0530

    Update Mutations.py

--- Strings/Mutations.py
+++ Strings/Mutations.py
@@ -1,7 +1,5 @@
 def mutate_string(string, position, character):
-    l = list(string)
-    l[position] = character
-    string = ''.join(l)
+    string=string[:position]+character+string[position+1:]
     return string
 
 if __name__ == '__main__':
