ccfa23b1d36a6f03d566c1e51cfbfb3eb27e2ee5
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2021-10-24 14:21:19 +0530

    Update Whats_your_name.py

--- Strings/Whats_your_name.py
+++ Strings/Whats_your_name.py
@@ -1,5 +1,15 @@
-def print_full_name(a, b):
-    print("Hello "+ a +" "+ b +"! You just delved into python.")
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello "+ first +" "+ last +"! You just delved into python.")
 
 if __name__ == '__main__':
     first_name = input()
