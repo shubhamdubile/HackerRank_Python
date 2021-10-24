93b1faaa35fa33958bb22185a9d0c81b83cc619a
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2021-10-24 12:05:36 +0530

    Update Print_Function.py

--- Introduction/Print_Function.py
+++ Introduction/Print_Function.py
@@ -1,3 +1,15 @@
+Solution:-
 n = int(input())
 for _ in range (1,n+1):
     print(_,end='')
+    
+    
+    
+Another Solution:-
+if __name__ == '__main__':
+    n = int(input())
+    list=[]
+    for i in range(1,n+1):
+        list.append(i)
+    print(''.join(str(e) for e in list))    
+    
