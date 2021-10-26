6478a1fd80cd37f7c5a841d7a123340bda557dd4
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2021-10-26 10:21:58 +0530

    Update Tuples.py

--- Basic_Data_Types/Tuples.py
+++ Basic_Data_Types/Tuples.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
-    tuples = tuple(integer_list)
-    print(hash(tuples))
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t=tuple(integer_list)
+    print(hash(t))
+
